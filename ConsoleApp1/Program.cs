995a08d61731f0df2d29db1290f3997205e4ac49
WKWAPISI <WKWAPISI@LCE28164>
2020-01-18 20:28:11 +0100

    Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using java.io;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class PdfReader
+    {
+        public string GetStringFromPdfStream(Stream stream)
+        {
+            PDDocument doc = null;
+            try
+            {
+                doc = PDDocument.load(new JavaIoWrapper(stream));
+                PDFTextStripper stripper = new PDFTextStripper();
+                return stripper.getText(doc);
+            }
+            finally
+            {
+                if (doc != null)
+                {
+                    doc.close();
+                }
+            }
+        }
+
+        public class JavaIoWrapper : InputStream
+        {
+            private Stream _underlyingStream;
+
+            public JavaIoWrapper(Stream stream)
+            {
+                _underlyingStream = stream;
+            }
+
+            public override int read()
+            {
+                return _underlyingStream.ReadByte();
+            }
+        }
+    }
+}
